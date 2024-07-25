a853dc08cd0aa4478805ac3dc894f8363d0f2520
Haghveisi.f <haghveisi.ftm@gmail.com>
2024-07-25 11:00:18 +0330

    commit message

--- main.cpp
+++ main.cpp
@@ -1,64 +1,63 @@
-#include <algorithm>
 #include <iostream>
-#include <iterator>
+#include <algorithm>
 #include <random>
 #include <vector>
 #include <unistd.h>
 
 using namespace std;
 
+
+vector<char> li{'1', '2', '3', '4', '5', '6', '7', '8', '1', '2', '3', '4', '5', '6', '7', '8'};
+vector<char> star{'*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*'};
 int num1,num2;
+
+void random_li()
+{
+    //It should be used only once
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(li.begin(), li.end(), g);
+}
 void line_list()
 {
-    vector<char> li{'1', '2', '3', '4', '5', '6', '7', '8', '1', '2', '3', '4', '5', '6', '7', '8'};
-    vector<char> star{'*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*'};
     int a=0;
+    cout<<"\n\n";
 
-        random_device rd;
-        mt19937 g(rd());
-        shuffle(li.begin(), li.end(), g);
+    for (char out_li : star)
+    {
+        a++;
 
-
-        cout<<"\n\n";
-        for (char out_li : star)
+        cout<<"| " << out_li <<" | ";
+        if(a == 4 )
         {
-            star[num1-1]=li[num1-1];
-            star[num2-1]=li[num2-1];
-
-            if(star[num1-1] != star[num2-1])
-            {
-                a++;
-
-                cout<<"| " << out_li <<" | ";
-                if(a == 4 )
-                {
-                    cout<<"\n-----------------------\n";
-                }
-
-                else if(a == 8)
-                {
-                    cout<<"\n-----------------------\n";
-                }
-
-                else if(a == 12)
-                {
-                    cout<<"\n-----------------------\n";
-                }
-            }
-
-            else
-            {
-                cout << "You can't choose a duplicate house !!!"<<endl;
-                break;
-            }
+            cout<<"\n-----------------------\n";
         }
 
-//        cout << "\n\n\n";
-//        for(int i=0; i<16; i++)
-//        {
-//            cout<<i+1<<" = "<<li[i]<<endl;
-//        }
-        cout << endl;
+        else if(a == 8)
+        {
+            cout<<"\n-----------------------\n";
+        }
+
+        else if(a == 12)
+        {
+            cout<<"\n-----------------------\n";
+        }
+    }
+    cout << "\n\n\n";
+
+
+//    for(int i=0; i<16; i++)
+//    {
+//        cout<<i+1<<" = "<<li[i]<<endl;
+//    }
+//
+//    cout << "\n\n\n";
+//    for(int i=0; i<16; i++)
+//    {
+//        cout<<i+1<<" = "<<star[i]<<endl;
+//    }
+//
+//    cout << endl;
 }
 
 void line_table()
@@ -84,24 +83,67 @@ void line_table()
 }
 
 
+
 int main()
 {
+    random_li();
+    line_table();
+
     while (true)
     {
-        line_table();
-
-        cout<<"\n\n number 1 : ";
+        lable : cout<<"\n\n number 1 : ";
         cin>>num1;
         cout<<"number 2 : ";
         cin>>num2;
+        if(num1 == num2)
+        {
+            cout << "You can't choose a duplicate house please try again !!!"<<endl;
+            goto lable;
+        }
 
+        if(star[num1-1] == li[num1-1] && star[num2-1] == li[num2-1])
+        {
+            cout<<"This house is full !!!!!\n\n";
+            goto lable;
+        }
+
+        star[num1-1]=li[num1-1];
+        star[num2-1]=li[num2-1];
 
         line_list();
-        cout << "\n\nSleeping for 5 seconds...\n";
-        sleep(5);
-        cout << "Done sleeping!\n";
 
+        if(li[num1-1] == li[num2-1])
+        {
+            star[num1-1]=li[num1-1];
+            star[num2-1]=li[num2-1];
+        }
+        else
+        {
+            cout << "\n\nSleeping for 5 seconds...\n";
+            sleep(5);
+            cout << "Done sleeping!\n";
+
+            star[num1-1] = '*' ;
+            star[num2-1] = '*' ;
+        }
+
+
+        int c = 0;
+
+        for (int i = 0 ; i <= 15 ; i++)
+        {
+            if (star[i] != li[i])
+            {
+                c++;
+                break;
+            }
+        }
+
+        if (c == 0)
+        {
+            cout << "\nThis game is over !!!" << endl;
+            return 0;
+        }
     }
-
     return 0;
 }
